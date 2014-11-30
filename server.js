36dcc47f37d35347d0d2a75637daf385b48690a8
Vincent Moore <catalystlive@hotmail.com>
2014-11-30 15:00:00 +0000

    blocked off routes and database

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ var express = require("express");
 var app = express();
 var http = require("http").Server(app);
 
-var session = require("express-session");
+//var session = require("express-session");
 var bodyParser = require("body-parser");
-var cookieParser = require("cookie-parser");
+//var cookieParser = require("cookie-parser");
 var methodOverride = require("method-override");
 var expressLayouts = require("express-ejs-layouts"); //test
 
 var config = require("./config");
-var passport = require("passport");
+//var passport = require("passport");
 
 //Configure Express
 console.log('Configuring Express....');
@@ -33,31 +33,35 @@ app.set( 'view engine', 'html' );
 
 app.use( expressLayouts );
 app.set("layout extractScripts", true);
-app.use( cookieParser() );
+//app.use( cookieParser() );
 app.use( bodyParser.json() );
 app.use( bodyParser.urlencoded( { extended: false } ) );
-app.use( session({
-    secret: config.session.secret,
-    saveUninitialized: true,
-    resave: true
-}));
-app.use( passport.initialize() );
-app.use( passport.session() );
+// app.use( session({
+//     secret: config.session.secret,
+//     saveUninitialized: true,
+//     resave: true
+// }));
+//app.use( passport.initialize() );
+//app.use( passport.session() );
 app.use( methodOverride() );
 
 //Initialize Passport
-console.log('Checking Passport...');
-require("./passport")(passport);
+//console.log('Checking Passport...');
+//require("./passport")(passport);
 
 //Initialize Routes
-console.log('Establishing Routes....');
-require("./routes")(app, passport);
+//console.log('Establishing Routes....');
+//require("./routes")(app, passport);
 
 //Initialize database
-console.log('Connecting to Database....');
-require("./database/db");
+//console.log('Connecting to Database....');
+//require("./database/db");
 
 console.log('Starting Server....');
 http.listen( config.env.port, function(){
     console.log('Listening to port:\t%s', config.env.port );
+});
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/public/views/park.html');
 });
