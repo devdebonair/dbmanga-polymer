43be55afb060cb9bff467a106d84c1e3a75b29cb
Vincent Moore <catalystlive@hotmail.com>
2015-03-25 14:40:14 +0000

    added news slider with glide with arrows and removed ajax request and replaced with server call

--- routes/general/home.js
+++ routes/general/home.js
@@ -1,11 +1,22 @@
-module.exports = function(router, passport, manga, user)
+module.exports = function(router, passport, manga, user, article)
 {
+    var recentArticles = null;
+    
+    article.find({}, '', { sort: { published: '-1' }, limit: 15 }, function(err, data){
+        if(err)
+        {
+            console.log(err);
+            return;
+        }
+        recentArticles = data;
+    });
+    
     router.route('/')
     
         .get(function(req, res){
-            
             res.render('partials/home', {
                 user: req.user,
+                articles: recentArticles,
                 meta:{ 
                     title: 'Debonair Manga - Responsive Online Manga Reader', 
                     description: 'Read Naruto, One Piece, Attack on Titan and many more manga on the best manga reading platform for free.',
