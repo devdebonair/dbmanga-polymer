3a783de09158afe093b60d469d75be7bd8b4794a
Vincent Moore <catalystlive@hotmail.com>
2015-03-26 05:17:56 +0000

    added suggestions to search

--- public/js/search.js
+++ public/js/search.js
@@ -25,4 +25,49 @@ $(function(){
         $('#banner form').css('display', 'none');
         $('#header').css('display', 'block');
     });
+    
+    var isFetched = false;
+    $('#search').keyup(function(){
+        var query = $(this).val();
+        
+        if(query.length === 0)
+        {
+            $('#main').css('display','block');
+            $('#search-results .row').empty();
+            isFetched = false;
+        }
+        
+        if(query.length < 3)
+        {
+            return;
+        }
+        
+        if(isFetched)
+        {
+            var queryRegex = new RegExp(query,'g');
+            $('#search-results .row').children().each(function(){
+                if(!queryRegex.test($(this).data('title')))
+                {
+                    $(this).css('display','none');
+                    return;
+                }
+                $(this).css('display','block');
+            });
+        }
+        
+        var queryString = '/api/v1/manga/search?title=' + query;
+        if(!isFetched)
+        {
+            isFetched = true;
+            $.getJSON(queryString, function(data){
+                var stringToAppend = '';
+                for(var i = 0; i < data.length; i++)
+                {
+                    stringToAppend += '<div class="col l2" data-title="'+ data[i].title +'"><a href="/manga/'+ data[i].title.replace(/ /g,'_') +'"><img height="150" width="240" class="responsive-img" src="'+ data[i].coverUrl +'"></a></div>';
+                }
+                $('#main').css('display','none');
+                $('#search-results .row').append(stringToAppend);
+            });
+        }
+    });
 });
